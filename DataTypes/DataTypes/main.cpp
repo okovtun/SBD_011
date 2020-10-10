475b33792600e76da39c1d61098c754564a6c17a
Clayman <Clayman@DESKTOP-649SK3N>
2020-10-10 18:35:38 +0300

    Constants

--- DataTypes/DataTypes/main.cpp
+++ DataTypes/DataTypes/main.cpp
@@ -1,10 +1,67 @@
 #include<iostream>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+//#define TYPES
+
+//Description - описание
+#define DESCRIPTION(type, min_value, max_value, umax_value)cout << "Переменная типа "#type" занимает " << sizeof(type) << " Байта памяти, и принимает значения в диапазоне:\n"\
+<< "unsigned "#type": " << 0 << "..." << umax_value << endl\
+<< "  signed "#type":" << min_value << "..." << max_value << endl
 
 void main()
 {
 	setlocale(LC_ALL, "Rus");
-	cout << (bool)0.000 << endl;
+	//cout << (bool)0.000 << endl;
+
+#ifdef TYPES
+	cout << "Переменная типа short занимает ";
+	cout << sizeof(short) << " Байта памяти, и принимает значения в диапазоне:\n";
+	cout << "unsigned ""short"": " << 0 << "..." << USHRT_MAX << endl;
+	cout << "  signed short:" << SHRT_MIN << "..." << SHRT_MAX << endl;
+
+	DESCRIPTION(long long, LLONG_MIN, LLONG_MAX, ULLONG_MAX);
+	DESCRIPTION(int, INT_MIN, INT_MAX, UINT_MAX);
+
+	//IA-16:
+	int a;
+
+	//IA-32:
+	short int b;
+	long int c;
+
+
+	cout << sizeof(double) << endl;
+	cout << DBL_MIN << "\t" << DBL_MAX << endl;
+#endif // TYPES
+
+
+	//int a;
+	//double cammelCaseStyle;
+	/*double price_of_coffee;
+	int number_of_cups;
+	cout << "Введите стоимость чашки кофе: ";	cin >> price_of_coffee;
+	cout << "Введите количество чашек: ";		cin >> number_of_cups;
+	double total_price = price_of_coffee * number_of_cups;
+	cout << "С Вас: "<< total_price << " р." << endl;
+	
+	double price;
+	double price;
+	cout << price;*/
+
+	//Проинициализировать
+	//Инициализация - это присвоение началоного значения
+
+	int speed = 0;
+	const int MAX_SPEED = 250;
+
+	const double SOME_CONST = .3f;
+
+	cout << typeid('+').name() << endl;
+	cout << '+' << endl;
+	cout << sizeof("Hello") << endl;
 }
 
 /*
@@ -26,5 +83,13 @@ void main()
 	long	4B
 	int		2-4B
 	long long 8B
+
+
+
+
+	//Identifier - это имя
+	1. ABC...Zabc...z0123456789_
+	2. 
+	3.
 ----------------------------------
 */
