fac87c0b9e89ba2f14e4129af83e9041cd37b0f5
Clayman <Clayman@DESKTOP-DM8E77U>
2021-02-20 15:10:27 +0200

    Parameters DONE.

--- Pointers/Parameters/main.cpp
+++ Pointers/Parameters/main.cpp
@@ -9,21 +9,38 @@ using namespace std;
 ----------------------------------
 */
 
-void Exchange(int* a, int* b);
+void Exchange(int& a, int& b);
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	int a = 2, b = 3;
 	cout << a << "\t" << b << endl;
-	Exchange(&a, &b);
+	Exchange(a, b);
 	cout << a << "\t" << b << endl;
 }
 
-void Exchange(int* a, int* b)
+void Exchange(int& a, int& b)
 {
-	int buffer = *a;
-	*a = *b;
-	*b = buffer;
+	int buffer = a;
+	a = b;
+	b = buffer;
 	cout << a << "\t" << b << endl;
 }
+
+/*
+----------------------------------------
+int - int;
+int* - Указатель на int;
+int& - Ссылка на int;
+double - double;
+double* - Указатель на double;
+double& - Ссылка на double;
+Car - Car (Машина);
+Car* - Указатель на Машину;
+Car& - Ссылка на машину;
+T - Шаблонный тип данных;
+T* - Указатель на шаблонный тип данных;
+T& - Ссылка на шаблонный тип данных.
+----------------------------------------
+*/
---
+++ Pointers/References/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+/*
+-------------------------
+Reference - это переменная, которая содержит адрес другой переменной.
+-------------------------
+*/
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a = 2;
+	int& ra = a;
+	ra += 3;
+	cout << &a << endl;
+	cout << &ra << endl;
+}
